f72bf01566db4eea87684b77aac0f604d661069e
AlejandraKeber <alejandrakeber@gmail.com>
2023-02-06 17:11:17 -0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 import './App.css';
 
 const App = () => (
   <div>
-    <Home />
+    <header className="App-header">
+      <Navbar />
+      <div className="line" />
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
@@ -8,8 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
